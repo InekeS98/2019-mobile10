f7811885cf4dee1c0ce6fae7f478cd6450eef075
INEKE <inekesusilowati89@gmail.com>
2019-11-08 15:33:31 +0700

    enggunakan ViewModel, tidak diperlukan proses pengambilan ulang atau kehilangan data pada UI Android.

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/fragments/VisitorFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,21 +22,19 @@ import id.ac.polinema.skorbasket.viewmodels.SharedScore;
  * A simple {@link Fragment} subclass.
  */
 public class VisitorFragment extends Fragment {
-
 	private Button scoreDuaVisitor;
 	private Button scoreTigaVisitor;
 	private Button scoreSatuVisitor;
 	private TextView scoreVisitor;
 	private SharedScore sharedScore;
 	private int scoreDefault = 0;
-
 	public VisitorFragment() {
 		// Required empty public constructor
 	}
-
 	@Override
 	public void onCreate(@Nullable Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		sharedScore = ViewModelProviders.of(requireActivity()).get(SharedScore.class);
 	}
 
 	@Override
@@ -43,7 +43,6 @@ public class VisitorFragment extends Fragment {
 		// Inflate the layout for this fragment
 		return inflater.inflate(R.layout.fragment_visitor, container, false);
 	}
-
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
@@ -52,7 +51,34 @@ public class VisitorFragment extends Fragment {
 		scoreVisitor = view.findViewById(R.id.scoreVisitor);
 		scoreSatuVisitor = view.findViewById(R.id.scoreSatuVisitor);
 
+		sharedScore.getScoreVisitor().observe(requireActivity(), new Observer<Integer>() {
+			@Override
+			public void onChanged(Integer score) {
+				scoreVisitor.setText(String.valueOf(score));
+				scoreDefault = score;
+			}
+		});
 
 		// Tambahkan logic tombol di bagian bawah ini
+		scoreDuaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+2);
+			}
+		});
+
+		scoreTigaVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+3);
+			}
+		});
+
+		scoreSatuVisitor.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				sharedScore.setScoreVisitor(scoreDefault+1);
+			}
+		});
 	}
 }
